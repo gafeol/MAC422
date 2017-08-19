cf9d9d20e9e1dcbca7f490b7b3189c76daa44f1e
Gabriel Fernandes <gabfeol@gmail.com>
2017-08-19 18:45:20 -0300

    arrumando data + iniciando chamada de ping

--- ep1sh.c
+++ ep1sh.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <time.h>
-#include <stdint.h>
-#define MST (-7)
-#define BRT (-3)
-#define UTC (0)
-#define CCT (+8)
 
-int main ()
+int main (int argc, char *argv[])
 {
-	time_t rawtime;
-	struct tm * ptm;
+	if(strcmp(argv[1], "date") == 0){
+		time_t result = time(NULL);
+		if(result != -1){
+			printf("%s", asctime(localtime(&result)));
+		}
+	}
+	else if(argv[1][0] == '/'){
+		int pid = fork();
+		if (pid=0){
+		    int err;
+		    char *env[1] = { 0 };
+		    char *argv[3] = { "ls", "-l", 0 };
+				err = execve("/bin/ls", argv, env);  //syscall, libc has simpler wrappers (man exec)
+		    exit(err); //if it got here, it's an error
+		} else if(pid<0) {
+		    printf("fork failed with error code %d\n", pid);
+		    exit(-1);
+		}
 
-	time ( &rawtime );
-
-	ptm = gmtime ( &rawtime );
-
-	printf ("Beijing (China) :     %2d:%02d\n", (ptm->tm_hour+CCT)%24, ptm->tm_min);
-
-	time_t result = time(NULL);
-
-	if(result != -1){
-		printf("%s", asctime(localtime(&result)));
+		int status;
+		wait(&status); //simplest one, man wait for others
+		printf("child pid was %d, it exited with %d\n", pid, status);
+		exit(0);
 	}
 }
