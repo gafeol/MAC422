19ae900073ff567e0298140f142ca7a8559ebbdf
Gabriel Fernandes <gabfeol@gmail.com>
2017-08-19 18:18:16 -0300

    local time

--- ep1sh.c
+++ ep1sh.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdint.h>
+#define MST (-7)
+#define BRT (-3)
+#define UTC (0)
+#define CCT (+8)
 
-int main(void){
+int main ()
+{
+	time_t rawtime;
+	struct tm * ptm;
+
+	time ( &rawtime );
+
+	ptm = gmtime ( &rawtime );
+
+	printf ("Beijing (China) :     %2d:%02d\n", (ptm->tm_hour+CCT)%24, ptm->tm_min);
+
 	time_t result = time(NULL);
-	if(result != -1)
-		printf("%s", asctime(gmtime(&result)));
+
+	if(result != -1){
+		printf("%s", asctime(localtime(&result)));
+	}
 }
