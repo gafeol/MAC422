ed53e3f627956842496ddb74999bd267e0f20da6
Gabriel Fernandes <gabfeol@gmail.com>
2017-08-20 18:12:14 -0300

    minor changes

--- ep1sh.c
+++ ep1sh.c
@@ -24,8 +24,13 @@ int main (int argc, char *argv[]){
 	else if(strcmp(argv[1], "/bin/ping") == 0){
 		pipe(pipe_arr);
 		if (fork() == 0){
+			//int err;
+			//char *env[1] = { "www.google.com.br" };
+			//char *argv[3] = {"ping"};
+			//err = execve("/bin/ping", argv, env);  //syscall, libc has simpler wrappers (man exec)
+			//exit(err); //if it got here, it's an error
 			dup2(pipe_arr[1], STDOUT);
-			execl("/bin/ping", "ping", argv[2], argv[3], argv[4], (char*)NULL);
+			execl("/bin/ping", "ping" argv[3](char*)NULL);
 		}
 		else {
 			wait(NULL);
@@ -47,6 +52,22 @@ int main (int argc, char *argv[]){
 			read(pipe_arr[0], buf, BUFLEN);
 			printf("%s\n", buf);
 		}
+
+		close(pipe_arr[0]);
+		close(pipe_arr[1]);
+	}
+	else if(strcmp(argv[1], "/usr/bin/cal") == 0){
+		pipe(pipe_arr);
+		if (fork() == 0){
+			dup2(pipe_arr[1], STDOUT);
+			execl("/usr/bin/cal", "cal", argv[2], (char*)NULL);
+		}
+		else {
+			wait(NULL);
+			read(pipe_arr[0], buf, BUFLEN);
+			printf("%s\n", buf);
+		}
+
 		close(pipe_arr[0]);
 		close(pipe_arr[1]);
 	}
