4707481f7468dbf59461d6e5512c0b74c34eda02
Thiago Estrela <thiagestrela@gmail.com>
2017-09-03 13:50:27 -0300

    Round Robin(Implementar Fila das CPU's disponiveis)

--- rr.c
+++ rr.c
@@ -1,15 +1,17 @@
 #include "process.h"
 #include "queue.h"
 #include "heap.h"
+#include <sys/time.h>
 
-static timeval start_time;
+typedef struct timeval timev;
+static timev start_time;
 
-double sec(timeval t){
+double sec(timev t){
 	return t.tv_sec + t.tv_usec/1000000.;
 }
 
 double running_time(){
-	timeval act;
+	timev act;
 	gettimeofday(&act, NULL);
 	return sec(act) - sec(start_time); 
 }
@@ -26,6 +28,8 @@ void SJF(FILE* input, FILE* output, int ncores){
 	}
 
 	Heap running_process = heap_create();
+	Queue awaiting_process = queue_create();
+	
 
 	gettimeofday(&start_time, NULL);
 	
@@ -36,18 +40,40 @@ void SJF(FILE* input, FILE* output, int ncores){
 	}
 
 	while(current_process != NULL){
-		while(!heap_empty(running_process) && running_time() >= heap_min_time(running_process)){
+		/*while(!heap_empty(running_process) && running_time() >= heap_min_time(running_process)){
 			// processo acabou de rodar
 			Process ready = 
-		}
-		while(current_time() < current_process->t0);
+		}*/
 
-		while(!heap_empty(ordered_process) && running_time() > heap_min_time(ordered_process)){
-			Process top = heap_min_element(ordered_process);
-			heap_push(next_process, top->dt, top);
+		while(current_time() < current_process->t0 && !heap_empty(running_process) && heap_min_time(running_process) <= running_time());
+
+		while(current_time() >= current_process->t0) {
+			double exe_time = 0;
+			if(current_process->dt >= quantum)
+					exe_time = quantum;
+				else
+					exe_time = current_process->dt;
+			queue_push(awaiting_process, exe_time, current_process);
+			if(heap_empty(ordered_process)) break;
+			current_process = heap_min_element(ordered_process);
 			heap_pop(ordered_process);
 		}
-		while(running_process->size < ncores && !heap_empty(next_process)){
+
+		while(!heap_empty(running_process) && running_time() > heap_min_time(running_process)){
+			Process top = heap_min_element(running_process);
+			if(top->dt > 0) {
+				double exe_time = 0;
+				if(top->dt >= quantum)
+					exe_time = quantum;
+				else
+					exe_time = top->dt;
+				top->dt -= exe_time;
+				queue_push(awaiting_process, exe_time, top);
+				heap_pop(running_process);
+			}
+		}
+
+		while(!(awaiting_process->size == 0) && !heap_empty(next_process)){
 			Process top = heap_min_element(next_process);
 			top->cpu running_process->size + 1;	
 			pthread_mutex_unlock(top->mutex);
