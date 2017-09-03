619f495c73f075cb61c46b200ea8e7626160fb93
Gabriel Fernandes <gabfeol@gmail.com>
2017-09-02 22:04:39 -0300

    esqueleto ep1

--- ep1.c
+++ ep1.c
@@ -1,14 +1,27 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<pthread.h>
 
 #define FL "trace.txt"	
 
 int main(int argc, char *argv[]){
-	FILE *fp;
-	fopen(FL, 
-	printf("Hello World!\n");
-	printf("%d parameters:\n", argc);
-	for(int a=0;a<argc;a++){
-		printf("%s\n", argv[a]);
-	}
+	int type = atoi(argv[1]);
+	FILE *trace = fopen(argv[2], "r"), *output = fopen(argv[3], "w");
+	switch (type) {
+		case 1:
+			//SJF(trace, output);
+			break;
+		case 2:
+			//RR(trace, output);
+			break;
+		case 3:
+			//P(trace, output);
+			break;
+		default:
+			fprintf(stderr, "Escalonador invalido\n");
+			break;
+	} 
+
+	fclose(trace); fclose(output);
+	return 0;
 }
