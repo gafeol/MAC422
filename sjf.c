4d272756c0d5ae4b7d633259dadc588b10a12feb
Gabriel Fernandes <gabfeol@gmail.com>
2017-09-03 12:32:17 -0300

    mudancas sjf

--- sjf.c
+++ sjf.c
@@ -25,25 +25,40 @@ void SJF(FILE* input, FILE* output, int ncores){
 		pthread_create(process->thread, NULL, run_process, process);
 	}
 
-	Heap next_process = heap_create();
 	Heap running_process = heap_create();
 
 	gettimeofday(&start_time, NULL);
+	
+	Process current_process = NULL;
+	if(!heap_empty(ordered_process)){
+		current_process = heap_min_element(ordered_process);
+		heap_pop(ordered_process);
+	}
 
-	while(!heap_empty(ordered_process) || !heap_empty(next_process)){
+	while(current_process != NULL){
+		while(!heap_empty(running_process) && running_time() >= heap_min_time(running_process)){
+			// processo acabou de rodar
+			Process ready = 
+		}
+		while(current_time() < current_process->t0);
+
 		while(!heap_empty(ordered_process) && running_time() > heap_min_time(ordered_process)){
 			Process top = heap_min_element(ordered_process);
 			heap_push(next_process, top->dt, top);
+			heap_pop(ordered_process);
 		}
 		while(running_process->size < ncores && !heap_empty(next_process)){
 			Process top = heap_min_element(next_process);
 			top->cpu running_process->size + 1;	
 			pthread_mutex_unlock(top->mutex);
 			heap_push(running_process, running_time() + top->dt, top);
+			heap_pop(next_process);
 		}
 
-
+		while(running_time() >= heap_min_time(running_process)){
+			Process ready = heap_min_element(running_process);
+			heap_pop(running_process);
+			Process next = 
+		}
 	}
 }
-
-
