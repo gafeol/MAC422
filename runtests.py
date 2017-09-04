ac9adfc5374e8bf79e6456606681e650e0099607
Thiago Estrela <thiagestrela@gmail.com>
2017-09-04 19:55:17 -0300

    runtests debug

--- runtests.py
+++ runtests.py
@@ -4,6 +4,6 @@ import os
 escalonador = int(sys.argv[1])
 nfiles = int(sys.argv[2])
 
-for i in range(1, nfiles):
+for i in range(1, nfiles+1):
 	print("Executando teste %d com escalonador %d" % (i, escalonador))
 	os.system("./ep1 %d tests/in%d.txt tests/out%d_%d.txt > results/dl%d_%d.txt" % (escalonador, i, i, escalonador, i, escalonador) )
