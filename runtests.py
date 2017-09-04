644bb23624c6d70bb434d46783776719c264d43b
Gabriel Fernandes <gabfeol@gmail.com>
2017-09-04 20:02:49 -0300

    minor bug runtests

--- runtests.py
+++ runtests.py
@@ -13,4 +13,4 @@ os.system("rm results/dl%d.txt" % escalonador)
 
 for i in range(0, nfiles):
 	print("Executando teste %d com escalonador %d" % (i, escalonador))
-	os.system("./ep1 %d tests/in%d.txt tests/out%d_%d.txt %d >> results/dl%d.txt" % (escalonador, i, escalonador, flag, i, escalonador) )
+	os.system("./ep1 %d tests/in%d.txt tests/out%d_%d.txt %d >> results/dl%d.txt" % (escalonador, i, escalonador, i, flag, i, escalonador) )
