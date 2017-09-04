7db115aa7f3f1f09392050646c6297583844844a
Gabriel Fernandes <gabfeol@gmail.com>
2017-09-04 18:40:28 -0300

    refinamento dos testes

--- gen.py
+++ gen.py
@@ -8,11 +8,11 @@ t0 = 0
 f = open('in', 'w+')
 
 while proc < n:
-	t0 += rnd.randrange (0, 20)/10.
+	t0 = rnd.randrange (0, 200)/10. ## processos comecam nos segundos de 0 a 19,1
 	t0 = int(round(t0*10))/10.;
-	dt = rnd.randrange (1, 30)/10.
+	dt = rnd.randrange (1, 50)/10. ## cada processo demora entre 0,1 e 4,9 segundos
 	dt = int(round(dt*10))/10.
-	deadline = rnd.randrange (1, 3) * dt + t0
+	deadline = rnd.randrange (10, 30)/10. * dt + t0
 	deadline = int(round(deadline*10))/10.
 	f.write(str(t0) + " " + str(dt) + " " + str(deadline) + " process"+ str(proc) + "\n")
 	proc += 1
