42e736ea08b7e12a29ad1783d50f09f875d72547
Gabriel Fernandes <gabfeol@gmail.com>
2017-09-04 19:59:24 -0300

    gerador 2.0

--- runtests.py
+++ runtests.py
@@ -2,8 +2,15 @@ import sys
 import os
 
 escalonador = int(sys.argv[1])
-nfiles = int(sys.argv[2])
+nfiles =int(sys.argv[2])
 
-for i in range(1, nfiles+1):
+flag = 0
+
+if len(sys.argv) > 3:
+	flag = int(sys.argv[3])
+
+os.system("rm results/dl%d.txt" % escalonador)
+
+for i in range(0, nfiles):
 	print("Executando teste %d com escalonador %d" % (i, escalonador))
-	os.system("./ep1 %d tests/in%d.txt tests/out%d_%d.txt > results/dl%d_%d.txt" % (escalonador, i, i, escalonador, i, escalonador) )
+	os.system("./ep1 %d tests/in%d.txt tests/out%d_%d.txt %d >> results/dl%d.txt" % (escalonador, i, escalonador, flag, i, escalonador) )
