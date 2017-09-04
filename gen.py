5bf4b01981fff26cd92e8ad7dafa082833013d21
Gabriel Fernandes <gabfeol@gmail.com>
2017-09-04 17:23:14 -0300

    generator with decimal numbers

--- gen.py
+++ gen.py
@@ -9,7 +9,10 @@ f = open('in', 'w+')
 
 while proc < n:
 	t0 += rnd.randrange (0, 20)/10.
+	t0 = int(round(t0*10))/10.;
 	dt = rnd.randrange (1, 30)/10.
+	dt = int(round(dt*10))/10.
 	deadline = rnd.randrange (1, 3) * dt + t0
+	deadline = int(round(deadline*10))/10.
 	f.write(str(t0) + " " + str(dt) + " " + str(deadline) + " process"+ str(proc) + "\n")
 	proc += 1
