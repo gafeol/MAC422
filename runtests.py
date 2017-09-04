7ca266a25e85662ea86be444a30799f8fb9a6b72
Gabriel Fernandes <gabfeol@gmail.com>
2017-09-04 19:37:22 -0300

    runtests

--- runtests.py
+++ runtests.py
@@ -6,4 +6,4 @@ nfiles = int(sys.argv[2])
 
 for i in range(1, nfiles):
 	print("Executando teste %d com escalonador %d" % (i, escalonador))
-	os.system("./ep1 %d tests/in%d.txt tests/out%d_%d.txt" % (escalonador, i, i, escalonador) )
+	os.system("./ep1 %d tests/in%d.txt tests/out%d_%d.txt > dl%d_%d.txt" % (escalonador, i, i, escalonador, i, escalonador) )
