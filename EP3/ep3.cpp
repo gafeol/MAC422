00e9ec8dc8ffef5c483747b2ffde9a960e51760f
gafeol <gabfeol@gmail.com>
2017-11-09 11:39:13 -0200

    inicio ep3

---
+++ EP3/ep3.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void roda(FILE *trace, int dt, int subs, int espaco){
+	FILE *mem, *vir;  
+	mem = fopen("tmp/ep3.mem", "r+wb"); 
+	vir = fopen("tmp/ep3.vir", "r+wb");
+	int buffer = -1;
+	int total, virt, ualoc, tamp;
+	fscanf(trace, "%d%d%d%d", &total, &virt, &ualoc, &tamp); 
+	printf("%d %d %d %d\n", total, virt, ualoc, tamp); 
+}
+
+int main(){
+	int dt;
+	int tipo_subs = 0, tipo_espaco = 0;
+	char input[30], file[110];
+	FILE *trace;
+	while(1){
+		printf("[ep3]: ");
+		scanf(" %s", input);
+		printf("'%s'\n", input);
+		if(strcmp(input, "sai") == 0)
+			break;
+		
+		if(strcmp(input, "carrega") == 0){
+			printf("carregou\n");
+			scanf(" %s", file);
+			printf("`%s`\n", file);
+			trace = fopen(file, "r");
+			printf("pa\n");
+		}
+		else if(strcmp(input, "substitui") == 0){
+			printf("substituiu\n");
+			scanf("%d", &tipo_subs);
+		}
+		else if(strcmp(input, "espaco") == 0){
+			scanf("%d", &tipo_espaco);
+		}
+		else if(strcmp(input, "executa") == 0){
+			printf("executa\n");
+			scanf("%d", &dt);
+			roda(trace, dt, tipo_subs, tipo_espaco);
+		}
+		else{
+			printf("Comando desconhecido\n");
+			break;
+		}
+	}
+}
